652a602a0b7e2475288ffbed52a5b5c67aad5d76
Marcy Acevedo <marcylinaacevedo@gmail.com>
2017-12-22 14:23:01 -0600

    changed the paddles syntax

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //written in vanilla JS. The reason being, I want to have a full grasp on it before I move onto jquery. I want to clearly understand both.
 
 
+
 //searches html doc for the canvas
 let canvas = document.querySelector ('canvas');
 
@@ -12,10 +13,10 @@ canvas.height = window.innerHeight;
 
 //code below is just practice for html canvas. just trying to get to know it. will likely not end up in //final code for the game.
 //creates ability to draw on canvas
-let context = canvas.getContext('2d');
+//let context = canvas.getContext('2d');
 
 //drawing a block. first two numbers are location in px on screen, width and H,  and the second two are shape's width and height
 //used this method to mock up a static game
-context.fillRect(100, 100, 20, 200);
-context.fillRect(1300, 400, 20, 200);
-context.fillRect(750, 300, 20, 20);
+//context.fillRect(100, 100, 20, 200);
+//context.fillRect(1300, 400, 20, 200);
+//context.fillRect(750, 300, 20, 20);
