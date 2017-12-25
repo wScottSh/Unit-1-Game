0a456324c1ed7c0eb77be560ebacf85b195e4af9
Marcy Acevedo <marcylinaacevedo@gmail.com>
2017-12-25 17:11:22 -0600

    positions player pieces, attempting player controls in JS. not successful yet..

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-//written in vanilla JS. The reason being, I want to have a full grasp on it before I move onto jquery. I want to clearly understand both.
+
+const player1 = getElementById('player1');
+const player2 = getElementById('player2');
+let player1Position = 0;
+let player2Position = window.innerWidth;
 
 
 
-//searches html doc for the canvas
-let canvas = document.querySelector ('canvas');
-
-//makes canvas full screen
-canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
-
-
-
-//code below is just practice for html canvas. just trying to get to know it. will likely not end up in //final code for the game.
-//creates ability to draw on canvas
-//let context = canvas.getContext('2d');
-
-//drawing a block. first two numbers are location in px on screen, width and H,  and the second two are shape's width and height
-//used this method to mock up a static game
-//context.fillRect(100, 100, 20, 200);
-//context.fillRect(1300, 400, 20, 200);
-//context.fillRect(750, 300, 20, 20);
+function playGame  () {
+  document.addEventListener('keypress', (event) => {
+    if (event.key === 'q') {
+      player1 = player1 -=15;
+    }
+    else if (event.key === 'a'){
+    player1 = player1 += 15;
+    }
+    else if (event.key === 'p'){
+      player2 = player2 -= 15;
+    }
+    else if (event.key === 'l'){
+      player2 = player2 += 15;
+    }
+playGame();
