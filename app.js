417eba562f70750956fdbfa519ee8ada9b8de43b
Marcy Acevedo <marcylinaacevedo@gmail.com>
2017-12-25 20:36:38 -0600

    threw all of original plan out the window and started over. hooray

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
+//defining variables(players, ball)
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+const player2 = document.getElementById('ball');
 
-const player1 = getElementById('player1');
-const player2 = getElementById('player2');
-let player1Position = 0;
-let player2Position = window.innerWidth;
+//starting position of players and ball
+//hard-coded position. could not use percentage for some reason. tried to have starting pos at 50%
 
+let player1Pos =770;
+let player2Pos = 887;
 
+//paddles move to right
+const playGame = () => {
+ document.addEventListener('keypress', (event) => {
+   if (event.key === 'z') {
+     player1Pos = player1Pos+ 20;
+            player1.setAttribute("style", "position: absolute; left: " + player1Pos + "px");
 
-function playGame  () {
-  document.addEventListener('keypress', (event) => {
-    if (event.key === 'q') {
-      player1 = player1 -=15;
-    }
-    else if (event.key === 'a'){
-    player1 = player1 += 15;
-    }
-    else if (event.key === 'p'){
-      player2 = player2 -= 15;
-    }
-    else if (event.key === 'l'){
-      player2 = player2 += 15;
-    }
+   } else if (event.key === 'm') {
+     player2Pos = player2Pos+ 20;
+            player2.setAttribute("style", "position: absolute; left: " + player2Pos + "px");
+
+   }
+ })
+}
+
 playGame();
