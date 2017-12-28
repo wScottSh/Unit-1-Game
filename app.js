86ef6e16e2a3402ba2cc190a25d86c33f29a4ddb
Marcy Acevedo <marcylinaacevedo@gmail.com>
2017-12-27 23:09:56 -0600

    game paddles are on board and ball is animated

--- app.js
+++ app.js
@@ -11,14 +11,17 @@
 //declare variables. This one was tricky to me. I wasn't sure when to use const or let, and I kept seeing confliction things, so I went
 //with let for everything. Logic being: games are dynamic. please let me know what would have been the proper way
 //to approach this situation.
-let canvas;
-let canvasContext;
-let ballX = 50;
-let ballHeight = 50;
+    let canvas;
+    let canvasContext;
+    let ballX = 50;
+    let ballY = 50;
 //this code makes the ball start at an angle, instead of straight across. 10 over and 4 down.
-let ballSpeedWidth =3;
-let ballSpeedHeight =1.5;
-let player1 = 250;
+    let ballSpeedX =3;
+    let ballSpeedY =1.5;
+    let player1 = 250;
+    let player2 = 250;
+    let padX = 15;
+    let padY = 100;
 //action to be taken when browser window opens
 window.onload = function() {
        //define var. grabs canvas id from html so it can be manipulated with JS
@@ -31,9 +34,7 @@ window.onload = function() {
            createGraphics();
            movement();
        },);
-       //invokes the functions.I am not sure if these should go here
-       createGraphics();
-       movement();
+
 }
 //function that creates actual gameboard and paddles and stuff
 let createGraphics = () => {
@@ -49,8 +50,9 @@ let createGraphics = () => {
        //all of your work with it when you finally do color it.
        //paddle 1
        canvasContext.fillStyle = 'green';
-       canvasContext.fillRect(0,player1,15,150);
-
+       canvasContext.fillRect(0,player1,padX,padY);
+       canvasContext.fillStyle = 'green';
+       canvasContext.fillRect(canvas.width - padX,player1,padX,padY);
        //paddle2
        //canvasContext.fillStyle = 'white';
        //canvasContext.fillRect(785,100,15,150);
@@ -61,30 +63,34 @@ let createGraphics = () => {
        canvasContext.beginPath();
        //creates circular shape for ball......There's no way in hell i would have ever figured this part out on my own. thanks, youTube! I read an article on radians and how a circle
        //is born afterward, to try to understand it better.I guess PI is half a circle? I'm not fully sure. True at the end means the circle is drawn clockwise. counter would be false.
-       canvasContext.arc(ballWidth,ballHeight,10,0,Math.PI*2, true);
+       canvasContext.arc(ballX,ballY,10,0,Math.PI*2, true);
        //fills in circle. there isnt a fillArc command so this command is used instead.
        canvasContext.fill();
 }
 //function to create functionality. i might move this to ABOVE graphics function. not sure if it matters yet
 let movement = () => {
-       ballWidth = ballWidth + ballSpeedWidth;
-       ballHeight = ballHeight + ballSpeedHeight;
+  //define movement variables
+       ballX = ballX + ballSpeedX;
+       ballY = ballY + ballSpeedY;
        //originally I had this hard-coded with  the ballSpeed from my above variable, but my programmer friend made me realize that it would be more
        //dry to use the variable name, so I wont need to write it again if speed changes or canvas size changes. This code basically says that when the ball hits a side of the canvas, to
        //bounce back to the opposite direction at the same speed.
-       if (ballWidth >= canvas.width) {
-           ballSpeedWidth = -ballSpeedWidth;
+       if (ballX >= canvas.width) {
+           ballSpeedX = -ballSpeedX;
        }
-   else if (ballWidth <= 0) {
-           ballSpeedWidth = -ballSpeedWidth;
+   else if (ballX <= 0) {
+           ballSpeedX = -ballSpeedX;
        }
        //this part made me crazy for a while. after i ran this code, my screen showed the ball vibrating on my screen instead of bouncing around.
        //after questioning everythung that is good in this world, I realized i had the  > and < incorrect. after i reversed them, it was fine, and my computer lived to
        //see another day
-   else    if (ballHeight <= 0) {
-           ballSpeedHeight = -ballSpeedHeight;
+   else    if (ballY<= 0) {
+           ballSpeedY = -ballSpeedY;
        }
-   else if (ballHeight >= canvas.height) {
-           ballSpeedHeight = -ballSpeedHeight;
+   else if (ballY >= canvas.height) {
+           ballSpeedY = -ballSpeedY;
        }
 }
+//invokes the functions.I am not sure if these should go here
+createGraphics();
+movement();
